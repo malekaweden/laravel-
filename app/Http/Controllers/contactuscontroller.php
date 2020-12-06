faf6b81df61786503bbad21b121f2c2d73ab6fbb
malekawedan <malekaweden30@gmail.com>
2020-12-06 21:36:42 +0200

    start api

---
+++ app/Http/Controllers/contactuscontroller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\contactmail;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+class contactuscontroller extends Controller
+{
+  
+    public function sendemail(Request $request){
+        $detalis=[
+            'name'=>$request->name,
+            'Company'=>$request->company,
+            'email'=>$request->email,
+            'massage'=>$request->msgg,
+        ];
+        Mail::to('malekaweden30@gmail.com')->send(new contactmail($detalis));
+        return response()->json(['success'=>true]);
+    }
+}
---
+++ resources/views/mail/contactmail.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>mail teapmple</title>
+</head>
+<body>
+<h1>{{$detalis['name']}}</h1>
+<h2>{{$detalis['company']}}</h2>
+<h3>{{$detalis['email']}}</h3>
+<h4>{{$detalis['msgg']}}</h4>
+</body>
+</html>
